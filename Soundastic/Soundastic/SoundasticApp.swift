ca67e286376cdf8b70342e8a154eca50cb9eb2cf
Lubos Lehota <lubos.lehota@cleverlance.com>
2024-06-01 17:03:05 +0200

    Initial empty project commit

---
+++ Soundastic/Soundastic/SoundasticApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundasticApp.swift
+//  Soundastic
+//
+//  Created by Lubos Lehota on 01/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct SoundasticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
