f87e0589491bd19af79b713ab83120cc969be116
Lubos Lehota <lubos.lehota@cleverlance.com>
2024-06-01 18:35:04 +0200

    Fix and format packages

--- Soundastic/Soundastic/App/Package.swift
+++ Soundastic/Soundastic/App/Package.swift
@@ -4,31 +4,31 @@
 import PackageDescription
 
 let package = Package(
-    name: "App",
-    platforms: [
-      .iOS("16")
-    ],
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "App",
-            type: .dynamic,
-            targets: ["App"]),
-    ],
-    dependencies: [
-      .package(path: "../Theme")
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "App",
-            dependencies: [
-              .product(name: "Theme", package: "Theme"),
-              .product(name: "Components", package: "Theme")
-            ]),
-        .testTarget(
-            name: "AppTests",
-            dependencies: ["App"]),
-    ]
+  name: "App",
+  platforms: [
+    .iOS("16")
+  ],
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "App",
+      type: .dynamic,
+      targets: ["App"]),
+  ],
+  dependencies: [
+    .package(path: "../Theme")
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "App",
+      dependencies: [
+        .product(name: "Theme", package: "Theme"),
+        .product(name: "Components", package: "Theme")
+      ]),
+    .testTarget(
+      name: "AppTests",
+      dependencies: ["App"]),
+  ]
 )
--- Soundastic/Soundastic/Theme/Package.swift
+++ Soundastic/Soundastic/Theme/Package.swift
@@ -4,31 +4,35 @@
 import PackageDescription
 
 let package = Package(
-    name: "Theme",
-    platforms: [
-      .iOS("16")
-    ],
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "Theme",
-            targets: ["Theme"]),
-        .library(
-            name: "Components",
-            targets: ["Components"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "Theme"),
-        .target(
-            name: "Components"),
-        .testTarget(
-            name: "ThemeTests",
-            dependencies: [
-              "Theme",
-              "Components"
-            ]),
-    ]
+  name: "Theme",
+  platforms: [
+    .iOS("16")
+  ],
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "Theme",
+      targets: ["Theme"]),
+    .library(
+      name: "Components",
+      targets: ["Components"]),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "Theme"),
+    .target(
+      name: "Components",
+      dependencies: [
+        "Theme"
+      ]
+    ),
+    .testTarget(
+      name: "ThemeTests",
+      dependencies: [
+        "Theme",
+        "Components"
+      ]),
+  ]
 )
